475232834692ac6a25d9e5ac2eb042823fe6bc89
Kevincamelo5 <92350827+Kevincamelo5@users.noreply.github.com>
2025-05-11 16:40:22 -0600

    mude la base de datos a postgre para que sea compatible con keroku

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,11 @@ login_manager.login_message_category = 'info'
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'tu_clave_secreta_aqui'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'    
+
+    # Configuración para PostgreSQL
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost/postgres'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+     
     db.init_app(app)
     login_manager.init_app(app)
 
--- run.py
+++ run.py
@@ -1,6 +1,13 @@
-from app import create_app
+from app import create_app, db
 
 app = create_app()
+
+with app.app_context():
+    try:
+        db.create_all()
+        print("Base de datos creada con éxito.")
+    except Exception as e:
+        print(f"Error al crear la base de datos: {e}")
 
 if __name__ == '__main__':
     app.run(debug=True)
