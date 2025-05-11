158e2da8d857d213d1574cdacb7dc88fee7f707d
Kevincamelo5 <92350827+Kevincamelo5@users.noreply.github.com>
2025-05-11 17:01:54 -0600

    borre la base de datos sqlite

--- app/__init__.py
+++ app/__init__.py
@@ -13,9 +13,9 @@ def create_app():
     app.config['SECRET_KEY'] = 'tu_clave_secreta_aqui'
 
     # Configuración para PostgreSQL
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost/postgres'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://postgres@localhost/site')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-     
+
     db.init_app(app)
     login_manager.init_app(app)
 
