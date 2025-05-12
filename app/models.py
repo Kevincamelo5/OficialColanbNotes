74dde385911384526ad525290f4fd0d0f606eb44
Kevincamelo5 <92350827+Kevincamelo5@users.noreply.github.com>
2025-05-12 12:23:40 -0600

    modifida las clases message

--- app/models.py
+++ app/models.py
@@ -15,6 +15,20 @@ user_contacts = db.Table(
     db.Column('contact_id', db.Integer, db.ForeignKey('user.id'), primary_key=True)
 )
 
+sent_private_messages = db.relationship(
+    'PrivateMessage',
+    foreign_keys='PrivateMessage.sender_id',
+    backref='sender_user',
+    lazy=True
+)
+
+received_private_messages = db.relationship(
+    'PrivateMessage',
+    foreign_keys='PrivateMessage.receiver_id',
+    backref='receiver_user',
+    lazy=True
+)
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
@@ -23,7 +37,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
 
     notes = db.relationship('Note', backref='author', lazy=True)
 
@@ -120,13 +134,13 @@ class FriendRequest(db.Model):
     sender = db.relationship('User', foreign_keys=[sender_id], back_populates='sent_friend_requests')
     receiver = db.relationship('User', foreign_keys=[receiver_id], back_populates='received_friend_requests')
 
+# En PrivateMessage
 class PrivateMessage(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text, nullable=False)
     sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     receiver_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)  # Marca de tiempo del mensaje
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
-    # Relaciones inversas
     sender = db.relationship('User', foreign_keys=[sender_id], backref='sent_private_messages')
     receiver = db.relationship('User', foreign_keys=[receiver_id], backref='received_private_messages')
